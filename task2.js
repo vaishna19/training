73518b4be63b0801f5962905f7e18fc1cc33e9b2
vaishna19 <77892336+vaishna19@users.noreply.github.com>
2021-03-16 23:44:24 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,44 @@
+//map
+// var a=[1,2,3,4];
+// var result=a.map((x)=>{
+// if (x%2===0)
+// return(x);
+// })
+// console.log(result);
+// var b=[1,3,5,7];
+// var hello=b.filter((x)=>{
+//     if(x>=3)
+//     return(x);
+// })
+// console.log(hello);
+// var a=[2,4,6,8];
+// var c=a.find((x)=>{
+//     if(x>2)
+//     return(x);
+// })
+// console.log(c);
+//findIndex
+// var a=[1,2,4,5,7];
+// var c=a.findIndex((x)=>{
+//     if(x===2)
+//     return(x);
+// })
+// console.log(c)
+//some
+// var a=[1,2,3,4];
+// var c=a.some((x)=>{
+//     if(x<0)
+//     return(x);
+// })
+// console.log(c);
+//every
+// var a=[1,4,5,7,8]
+// var c=a.every((x)=>{
+// return x%3===0
+// })
+// console.log(c);
+// var a=[1,2,3,4,5]
+// var c=a.forEach((x)=>{
+//     return x===3
+// })
+// console.log(c);
