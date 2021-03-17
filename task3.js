625b8c5fcc26b9ce0119f9c3c4905f9eb6857223
vaishna19 <77892336+vaishna19@users.noreply.github.com>
2021-03-17 23:38:10 +0530

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,31 @@
+//var fruits=["Orange","Kiwi","Banana","Jackfruit"];
+// console.log(fruits.toString());
+// console.log(fruits.join("*"));
+// fruits.pop(); 
+// console.log(fruits);
+// fruits.push("guava");
+// console.log(fruits);
+// var x=fruits.shift();
+//var x=fruits.unshift("Grapes");
+// console.log (fruits);
+// var x=[1,'1','name',[1,2],true];
+// delete fruits[0];
+// console.log(fruits);
+// var x=fruits.splice(2,0,"mulberry");
+// console.log(fruits);
+//console.log(fruits.slice(1));
+// fruits.sort();
+// console.log(fruits);
+//console.log(fruits.reverse());
+// var a=[0,1,4,2,7,8];
+//  a.sort(function (a,b) {return a-b});
+// console.log(a);
+// var a=[0,1,4,2,7,8];
+//   a.sort(function (a,b) {return b-a});
+//  console.log(a);
+// var a=[1,'1','name',[1,2],true];
+// var result=a.map((x)=>{
+//     return(typeof(x));
+// })
+
+// console.log(result);
