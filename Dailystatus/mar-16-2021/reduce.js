523e3f934fe655958b76591c0fb5b25959770f5b
vaishna19 <77892336+vaishna19@users.noreply.github.com>
2021-03-18 11:35:07 +0530

    Add files via upload

---
+++ Dailystatus/mar-16-2021/reduce.js
@@ -0,0 +1,8 @@
+// var a = [34, 45, 56, 13, 56];
+// var res = a.reduce((acc, ele) => {
+//     const b = ele % 2 === 0 ? 'even' : 'odd';
+//     acc[b].push(ele);
+//     return acc;
+// }, { odd: [], even: [] });
+// console.log(res);
+
