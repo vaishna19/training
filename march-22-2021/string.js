6e0e3f60438861c15965a98d02f8f9167f1294e8
vaishna19 <77892336+vaishna19@users.noreply.github.com>
2021-03-22 14:54:41 +0530

    Add files via upload

---
+++ march-22-2021/string.js
@@ -0,0 +1,44 @@
+var a="vaishnavi";
+var result=a.length;
+console.log(result);
+
+find a string in a string
+
+var b = "hello everyone i welcome each and everyone to join our 'healthcare services' "
+var result = b.indexOf("healthcare services");
+console.log(result);
+
+var b = "hello everyone i welcome each and everyone to join our 'healthcare services' "
+var result = b.lastIndexOf("healthcare");
+console.log(result);
+
+ replacing a string
+var str = "Please visit our nest!";
+var n = str.replace("nest", "new house");
+console.log(n);
+
+var a = "Please visit nest!";
+var n = str.replace(/NEST/i, "our house");
+console.log(n)
+
+var a="vaishu"
+var b=a.toUpperCase();
+console.log(b)
+
+var a="VAISHU"
+var b = a.toLowerCase();
+console.log(b);
+
+var a = "vaishu"
+var b = "jasper"
+console.log(a.concat(b));
+
+var a = "    hello vaish     "
+console.log(a.trim());
+
+let a = "8";
+console.log(a.padStart(4,0));
+
+let a = "8";
+console.log(a.padEnd(5,0));
+
